2b090f882c0109bbbdbeb8fc6413ed397fdc868c
Samuel Klas <klassam95@gmail.com>
2024-07-26 17:00:16 +0300

    init

---
+++ src/main/java/fi/samklas/trainbuddy/TrainbuddyApplication.java
@@ -0,0 +1,13 @@
+package fi.samklas.trainbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrainbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrainbuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fi/samklas/trainbuddy/TrainbuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package fi.samklas.trainbuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrainbuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
